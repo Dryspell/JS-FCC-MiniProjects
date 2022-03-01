cdde290e934417803c4bdb77f6626f844dfa3875
Dryspell <Nhwinslow@gmail.com>
2022-03-01 17:45:08 -0500

    Added checking for cubies off grid, no errors

--- Tetris/app.js
+++ Tetris/app.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded',()=> {
     const oTet = [[0,0],[1,0],[0,1],[1,1]];
     const sTet = [[0,2],[1,2],[1,1],[2,1]];
     const TETS = {j: jTet, i: iTet, o: oTet, s: sTet};
+    const tetColorPalette = ['red', 'blue', 'purple', 'yellow', 'green'];
 
     function rotate(piece, theta){
         let tPrime = math.unit(theta, 'deg');
@@ -49,13 +50,45 @@ document.addEventListener('DOMContentLoaded',()=> {
         };
     };
 
-    function draw(piece,color){
-        piece.forEach((cubie) => {
-            let gridSquare = document.getElementById(`Cell(${cubie[0]},${cubie[1]})`);
-            gridSquare.classList.add(`cubie-${color}`);
-            gridSquare.style.backgroundColor = color;
+    function getRandomPiece(){
+    // Tetrominoes are of the scheme: 
+        let piece = {
+            color: tetColorPalette[Math.floor(Math.random() * tetColorPalette.length)],
+            type: Object.keys(TETS)[Math.floor(Math.random() * Object.keys(TETS).length)],
+            loc: [4,1],
+            rot: math.floor(math.random()*4) * 90,
+            cubies: []
+        };
+        //console.log(piece);
+        piece.cubies = rotate(TETS[piece.type], piece.rot);
+        piece.cubies = piece.cubies.map((cubie) => math.add(cubie,piece.loc));
+        console.log(piece);
+        return piece;
+    }
+
+    function draw(piece){
+        //console.log(piece);
+        piece.cubies.forEach((cubie) => {
+            try {
+                let gridSquare = document.getElementById(`Cell(${cubie[0]},${cubie[1]})`);
+                gridSquare.classList.add(`cubie-${piece.color}`);
+                gridSquare.style.backgroundColor = piece.color;
+            } catch (TypeError) {
+                console.log(`Cubie ${cubie} doesn't fit in the grid`);
+            }
         });
     };
-    draw(jTet,'red');
-    draw(oTet,'blue');
+
+    function unDraw(piece){
+        piece.cubies.forEach((cubie) => {
+            let gridSquare = document.getElementById(`Cell(${cubie[0]},${cubie[1]})`);
+            gridSquare.classList.remove(`cubie-${color}`);
+        });
+    }
+
+    draw(getRandomPiece());
+    draw(getRandomPiece());
+    draw(getRandomPiece());
+    draw(getRandomPiece());
+    draw(getRandomPiece());
 })
