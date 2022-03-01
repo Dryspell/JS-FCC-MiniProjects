5bbbfb558ffff6c97b61b7072107df1dfa49951c
Dryspell <Nhwinslow@gmail.com>
2022-03-01 15:30:36 -0500

    Updated Tetris with dynamically colored pieces

--- Tetris/app.js
+++ Tetris/app.js
@@ -1,8 +1,6 @@
 
 
 document.addEventListener('DOMContentLoaded',()=> {
-    
-
     const gridContainer = document.getElementById("gridContainer");
     const scoreDisplay = document.querySelector('#score');
     const startBtn = document.querySelector('#start-button');
@@ -21,15 +19,13 @@ document.addEventListener('DOMContentLoaded',()=> {
         
         return piece.map((cubie) => math.round(math.multiply(cubie, rotMat)));
     }
-    console.log(rotate(jTet, 90));
+    //console.log(rotate(jTet, 90));
 
-    // let cells = document.getElementsByClassName("cell");
-
-    defaultGrid();
+    defaultGrid(10,20);
     //Creates a default grid sized 16x16 
-    function defaultGrid() {
-        makeRows(20);
-        makeColumns(10);
+    function defaultGrid(x,y) {
+        makeRows(y);
+        makeColumns(x);
     }
 
     //Takes (rows, columns) input and makes a grid
@@ -43,17 +39,23 @@ document.addEventListener('DOMContentLoaded',()=> {
 
     //Creates columns
     function makeColumns(cellNum) {
-        console.log(cellNum)
         let rows = document.getElementsByClassName("gridRow");
-        console.log(rows.length)
         for (i = 0; i < rows.length; i++) {
             for (j = 0; j < cellNum; j++) {
                 let newCell = document.createElement("div");
-                newCell.id = `Cell (${j},${i})`;
+                newCell.id = `Cell(${j},${i})`;
                 rows[i].appendChild(newCell).className = "cell";
-                //console.log([i,j])
             };
-
         };
     };
+
+    function draw(piece,color){
+        piece.forEach((cubie) => {
+            let gridSquare = document.getElementById(`Cell(${cubie[0]},${cubie[1]})`);
+            gridSquare.classList.add(`cubie-${color}`);
+            gridSquare.style.backgroundColor = color;
+        });
+    };
+    draw(jTet,'red');
+    draw(oTet,'blue');
 })
